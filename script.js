a62e913f70f2e679bb4dab67429a9831c9519302
ZingerEngineer <ramyisez@gmail.com>
2021-09-28 21:58:10 +0200

    Simple digital clock with time formats

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 let container = document.querySelector(".container");
-let timeSystem = "12-hours";
+let timeSystem = "12 hours";
 let d = new Date();
 let H = d.getHours();
 let M = d.getMinutes();
 let S = d.getSeconds();
+let K = ""
 let clock = [H, M, S];
 container.innerHTML = clock.join(":");
 let button = document.querySelector("#btn");
 button.innerHTML = timeSystem;
 button.addEventListener("click", () => {
   if (button.innerHTML == timeSystem) {
-    button.innerHTML = "24-hours";
+    
+    button.innerHTML = "24 hours";
+    if(H <= 24){ K = "PM"}
+    else if (H >=0){K = "AM"}
     H = Math.abs(d.getHours() - 12)
+    
   } else if (button.innerHTML != timeSystem) {
-    button.innerHTML = "12-hours";
+    button.innerHTML = "12 hours";
     H = d.getHours();
+    K = ""
   }
 });
 setInterval(() => {
   d = new Date();
   M = d.getMinutes();
   S = d.getSeconds();
-  clock = [H, M, S];
+  clock = [H,M,S]
   clock = clock
     .map((item) => String(item))
     .map((item) => (item.length >= 2 ? item : "0" + item));
-  container.innerHTML = clock.join(":");
+  container.innerHTML = clock.join(":") + K;
 }, 1000);
