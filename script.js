699216ebb62ab8109c5659304051325dc1b75bd9
ZingerEngineer <ramyisez@gmail.com>
2021-09-28 16:13:50 +0200

    halfdoneclock

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
-var container = document.querySelector(".container");
-var timeSystem = '12-hours';
-var d = new Date(); // for now
-var H = d.getHours(); // => 9
-var M = d.getMinutes(); // =>  30
-var S = d.getSeconds(); // => 51
+let container = document.querySelector(".container");
+let timeSystem = '12-hours';
+let d = new Date();
+let H = d.getHours();
+let M = d.getMinutes();
+let S = d.getSeconds();
+let clock = [H , M , S]
+container.innerHTML = clock.join(":")
+let button = document.querySelector(".btn") 
+   
+setInterval(() => {
+     d = new Date();
+     H = d.getHours();
+     M = d.getMinutes();
+     S = d.getSeconds();
+     clock = [H,M,S]
+    clock = clock
+        .map(item => String(item))
+        .map(item => item.length >= 2 ? item : '0' + item)
+     container.innerHTML = clock.join(":")
+},1000)
 
-var dynclock = setInterval(()=>{new Date()},1000)
-
-console.log(dynclock)
-
-/* console.log(d);
-var button = document.querySelector(".timeformat");
-var hours = d.getHours();
-var hours12 = Math.abs(d.getHours()-12)
-var mins = d.getMinutes();
-var sec = d.getSeconds();
-var clock = document.querySelector(".clock");
-button.innerHTML = timeSystem;
-button.addEventListener("click", () => {
-    if(timeSystem == "12-hours"){
-        timeSystem ="24-hours" 
-        button.innerHTML = timeSystem;
-        clock.innerHTML == hours12;
-    }
-    else{
-        timeSystem ="12-hours" 
-        button.innerHTML = timeSystem;
-
-    }
-}) */
