e29bd4b29959da3d612ab42aa7e6376adb4b44b2
ZingerEngineer <ramyisez@gmail.com>
2021-09-28 20:57:51 +0200

    clock done Lacking am/pm

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 let container = document.querySelector(".container");
-let timeSystem = '12-hours';
+let timeSystem = "12-hours";
 let d = new Date();
 let H = d.getHours();
 let M = d.getMinutes();
 let S = d.getSeconds();
-let clock = [H , M , S]
-container.innerHTML = clock.join(":")
-let button = document.querySelector(".btn") 
-   
+let clock = [H, M, S];
+container.innerHTML = clock.join(":");
+let button = document.querySelector("#btn");
+button.innerHTML = timeSystem;
+button.addEventListener("click", () => {
+  if (button.innerHTML == timeSystem) {
+    button.innerHTML = "24-hours";
+    H = Math.abs(d.getHours() - 12)
+  } else if (button.innerHTML != timeSystem) {
+    button.innerHTML = "12-hours";
+    H = d.getHours();
+  }
+});
 setInterval(() => {
-     d = new Date();
-     H = d.getHours();
-     M = d.getMinutes();
-     S = d.getSeconds();
-     clock = [H,M,S]
-    clock = clock
-        .map(item => String(item))
-        .map(item => item.length >= 2 ? item : '0' + item)
-     container.innerHTML = clock.join(":")
-},1000)
-
+  d = new Date();
+  M = d.getMinutes();
+  S = d.getSeconds();
+  clock = [H, M, S];
+  clock = clock
+    .map((item) => String(item))
+    .map((item) => (item.length >= 2 ? item : "0" + item));
+  container.innerHTML = clock.join(":");
+}, 1000);
